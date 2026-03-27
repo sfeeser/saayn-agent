ae9e39f0ac947c16ccbbf3a005aa5a446225e497
Stuart Feeser <sfeeser@gmail.com>
2026-03-26 20:27:27 -0400

    Create root.go

---
+++ cmd/saayn/root.go
@@ -0,0 +1,30 @@
+package saayn
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	genomeFile string
+	projectRoot string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "saayn",
+	Short: "SAAYN Code Genome System (CGS)",
+	Long: `A deterministic, AST-based mutation and repair engine 
+that treats code as a living genome.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&genomeFile, "genome", "g", "genome.json", "path to the CGS registry file")
+	rootCmd.PersistentFlags().StringVarP(&projectRoot, "path", "p", ".", "root directory of the project to scan")
+}
